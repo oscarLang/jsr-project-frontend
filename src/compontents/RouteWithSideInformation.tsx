2f37636de716438944b5a5448db2b7ccd1528415
Oscar Lang <oscar.lang98@gmail.com>
2021-10-29 15:24:17 +0200

    change gitignore

--- src/compontents/RouteWithSideInformation.tsx
+++ src/compontents/RouteWithSideInformation.tsx
@@ -17,13 +17,13 @@ const RouteWithSideInformation: React.FC<RouteProps> = (props: RouteProps) => {
             <Grid item xs={12} sm={7} >
                 <Route {...props} />
             </Grid>
-            <Grid container item xs={12} sm={4} gap={1}>
+            <Grid item xs={12} sm={4} gap={1}>
                 <Grid item sx={{flexGrow: 1, display: { xs: `${!isLoggedIn ? "none" : "inline"}`, sm: 'inline' }}}>
                     <Paper sx={{padding: "1em"}}>
                         {(!isLoggedIn) ? (<Login />) : (<Profile user={profile} funds={funds}/>)}
                     </Paper>
                 </Grid> 
-                <Grid item sx={{flexGrow: 1}}>
+                <Grid item sx={{marginTop: 2, flexGrow: 1}}>
                     <Paper sx={{padding: "1em"}}>
                         <EarningsCalendar />
                     </Paper>
